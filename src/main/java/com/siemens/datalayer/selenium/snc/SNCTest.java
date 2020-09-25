8ea11b326bb1ef7fec1932fac2e94523b7230dd6
Yu Feng <fengyu@siemens.com>
2020-09-25 07:01:40 +0000

    add snc cases

--- src/main/java/com/siemens/datalayer/selenium/snc/SNCTest.java
+++ src/main/java/com/siemens/datalayer/selenium/snc/SNCTest.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import com.siemens.datalayer.utils.AllureEnvironmentPropertiesWriter;
 import com.siemens.datalayer.utils.CustomizedTestListener;
 import com.siemens.datalayer.utils.Utils;
@@ -14,10 +15,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -28,12 +26,16 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 //@Listeners({TestngListener.class})
 @Listeners({CustomizedTestListener.class})
 public class SNCTest extends WebDriverBaseClass {
 
     public WebDriver driver;
     private String baseUrl;
+    private int uiPort;
     private int apiPort;
     private String uiBaseUrl;
 
@@ -42,7 +44,8 @@ public class SNCTest extends WebDriverBaseClass {
         WebDriverBaseClass bs = new WebDriverBaseClass();
         driver = bs.initialize_driver();
         baseUrl = "http://140.231.89.85";
-        uiBaseUrl = baseUrl + ":32189/";
+        uiPort = 32189;
+        uiBaseUrl = baseUrl + String.format(":%s/", String.valueOf(uiPort));
         apiPort = 31918;
 
         driver.get(uiBaseUrl);
@@ -54,7 +57,7 @@ public class SNCTest extends WebDriverBaseClass {
 
     @Test(description = "Verify the user login function")
     @Description("Verify user login process")
-    @Epic("EP001 Home Page Layout")
+    @Epic("SNC UI")
     @Feature("Feature2: Login")
     @Step("Check the Login function with correct password")
     @Severity(SeverityLevel.CRITICAL)
@@ -67,7 +70,7 @@ public class SNCTest extends WebDriverBaseClass {
 
     @Test(description = "Verify the page1 function")
     @Description("Verify the page1 function")
-    @Epic("EP001 Home Page Layout")
+    @Epic("SNC UI")
     @Feature("Feature2: Page1")
     @Step("Check page1 values")
     @Severity(SeverityLevel.CRITICAL)
@@ -94,7 +97,7 @@ public class SNCTest extends WebDriverBaseClass {
 
     @Test(description = "Verify the page2 function")
     @Description("Verify the page2 function")
-    @Epic("EP001 Home Page Layout")
+    @Epic("SNC UI")
     @Feature("Feature2: Page2")
     @Step("Check page2 values")
     @Severity(SeverityLevel.CRITICAL)
@@ -115,6 +118,46 @@ public class SNCTest extends WebDriverBaseClass {
 
         String text = driver.findElement(By.id("pp")).getText();
         Assert.assertTrue(text.length() > 10);
+
+//        WebElement canvas = driver.findElement(By.cssSelector("#pd canvas"));
+//        Actions builder = new Actions(driver);
+//        builder.moveToElement(canvas,652,250)  // start point
+//                .click()
+//                .perform();
+//        System.out.println("finish");
+    }
+
+
+    @Test(description = "Verify the page3 function")
+    @Description("Verify the page3 function")
+    @Epic("SNC UI")
+    @Feature("Feature2: Page3")
+    @Step("Check page3 values")
+    @Severity(SeverityLevel.CRITICAL)
+    public void page3UITest() {
+        this.loginAndCheckSuccess();
+        this.switchToProductDevice();
+        driver.switchTo().frame(0);
+        waitForAjaxToFinish();
+        String actual = driver.findElement(By.id("opproduce")).getText();
+        String plan = driver.findElement(By.id("opplan")).getText();
+        String pattern = "\\d+";
+
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(actual);
+        Assert.assertTrue(countMatchSize(m) == 2);
+        Matcher m2 = r.matcher(plan);
+        Assert.assertTrue(countMatchSize(m2) == 2);
+
+        String text = driver.findElement(By.id("pp")).getText();
+        Assert.assertTrue(text.length() > 10);
+
+//        WebElement canvas = driver.findElement(By.cssSelector("#pd canvas"));
+//        Actions builder = new Actions(driver);
+//        builder.moveToElement(canvas,652,250)  // start point
+//                .click()
+//                .perform();
+//        System.out.println("finish");
     }
 
     @Test(description = "Verify the apis used in page1 function")
@@ -127,6 +170,17 @@ public class SNCTest extends WebDriverBaseClass {
         this.verifySNCDemoBackendOfPage1();
     }
 
+
+    @Test(description = "Verify the apis used in page3 function")
+    @Description("Verify the apis used in page3 function")
+    @Epic("EP001 Home Page Layout")
+    @Feature("Feature2: Page3")
+    @Step("Check apis used in page3")
+    @Severity(SeverityLevel.CRITICAL)
+    public void page3ApiTest() {
+        this.verifySNCDemoBackendOfPage3();
+    }
+
     private int countMatchSize(Matcher m){
         List<String> allMatches = new ArrayList<String>();
         while (m.find()) {
@@ -135,7 +189,7 @@ public class SNCTest extends WebDriverBaseClass {
         return allMatches.size();
     }
 
-    public Response sncDemoBackend(String body) {
+    public Response sncDemoGraphQLControllerApi(String body) {
 
         RestAssured.baseURI = baseUrl;
         RestAssured.port = apiPort;
@@ -147,6 +201,118 @@ public class SNCTest extends WebDriverBaseClass {
                 .post("/graphqlController");
 
         return response;
+    }
+
+    public Response sncDemoGraphQLApi(String body) {
+
+        RestAssured.baseURI = baseUrl;
+        RestAssured.port = uiPort;
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.header("Content-Type", "text/plain")
+                .body(body)
+                .post("/graphql");
+
+        return response;
+    }
+
+    public Response sncDemoStatisticApi() {
+
+        RestAssured.baseURI = baseUrl;
+        RestAssured.port = uiPort;
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.header("Content-Type", "application/json")
+                .get("/snc/devices/statistic");
+
+        return response;
+    }
+
+    public Response sncDemoAbnormalApi() {
+
+        RestAssured.baseURI = baseUrl;
+        RestAssured.port = uiPort;
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.header("Content-Type", "application/json")
+                .get("/snc/devices/abnormal");
+
+        return response;
+    }
+
+
+    public void verifySNCDemoBackendOfPage3(){
+        this.verifySNCGraphQLApi();
+        this.verifySNCStatisticApi();
+        this.verifySNCAbnormalApi();
+    }
+
+    public void verifySNCGraphQLApi(){
+        String body = "{\n" +
+                "  Work_Center(cond: \"\", order: \"\") {\n" +
+                "\tname\n" +
+                "\tdescription\n" +
+                "\tlocation\n" +
+                "\tid\n" +
+                "\tContains_Work_Position(cond: \"\", order: \"\") {\n" +
+                "\t  device_id\n" +
+                "\t  procedure_id\n" +
+                "\t  name\n" +
+                "\t  description\n" +
+                "\t  work_center_id\n" +
+                "\t  location\n" +
+                "\t  invert_Device(cond: \"\", order: \"\") {\n" +
+                "\t\tid\n" +
+                "\t\tname\n" +
+                "\t\tinvert_KPI {\n" +
+                "\t\t  val\n" +
+                "\t\t  type\n" +
+                "\t\t  unit\n" +
+                "\t\t  device_id\n" +
+                "\t\t  start_time\n" +
+                "\t\t  end_time\n" +
+                "\t\t}\n" +
+                "\t  }\n" +
+                "\t}\n" +
+                "  }\n" +
+                "}";
+        Response response = this.sncDemoGraphQLApi(body);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+
+        Assert.assertNotNull(jsonPathEvaluator.get("data"));
+        ArrayList<HashMap> data = (ArrayList)jsonPathEvaluator.get("data.Work_Center");
+
+        Assert.assertEquals(3, data.size());
+        assertThat(response.getBody().asString(),
+                matchesJsonSchemaInClasspath("snc-graphql-schema.json"));
+    }
+
+    public void verifySNCStatisticApi(){
+        Response response = this.sncDemoStatisticApi();
+        JsonPath jsonPathEvaluator = response.jsonPath();
+
+        Assert.assertNotNull(jsonPathEvaluator.get("data"));
+        ArrayList<HashMap> data = (ArrayList)jsonPathEvaluator.get("data");
+
+        Assert.assertTrue(data.size() > 0);
+        assertThat(response.getBody().asString(),
+                matchesJsonSchemaInClasspath("snc-statistic-schema.json"));
+    }
+
+
+    public void verifySNCAbnormalApi(){
+        Response response = this.sncDemoAbnormalApi();
+        JsonPath jsonPathEvaluator = response.jsonPath();
+
+        Assert.assertNotNull(jsonPathEvaluator.get("data"));
+        ArrayList<HashMap> data = (ArrayList)jsonPathEvaluator.get("data");
+
+//        Assert.assertTrue(data.size() > 0);
+        assertThat(response.getBody().asString(),
+                matchesJsonSchemaInClasspath("snc-abnormal-schema.json"));
     }
 
     public void verifySNCDemoBackendOfPage1(){
@@ -190,7 +356,7 @@ public class SNCTest extends WebDriverBaseClass {
     }
 
     public void verifySNCDemoBackendReturnValue(String body, String key){
-        Response response = this.sncDemoBackend(body);
+        Response response = this.sncDemoGraphQLControllerApi(body);
 
         JsonPath jsonPathEvaluator = response.jsonPath();
 
@@ -227,6 +393,11 @@ public class SNCTest extends WebDriverBaseClass {
     private void switchToProductPlan(){
         driver.switchTo().defaultContent();
         driver.findElement(By.id("process")).click();
+    }
+
+    private void switchToProductDevice(){
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("product-device")).click();
     }
 
     public void waitForAjaxToFinish() {
