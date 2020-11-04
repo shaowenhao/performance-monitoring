d62acfc2b9295b8fe20c30208d02cd46852a56c9
Zang Di z003n44a <di.zang@siemens.com>
2020-11-04 10:51:24 +0800

    iot portal

--- src/main/java/com/siemens/datalayer/selenium/test/IoTPortalTest.java
+++ src/main/java/com/siemens/datalayer/selenium/test/IoTPortalTest.java
@@ -4,12 +4,17 @@ import com.siemens.datalayer.utils.AllureEnvironmentPropertiesWriter;
 import com.siemens.datalayer.utils.CustomizedTestListener;
 import com.siemens.datalayer.utils.WebDriverBaseClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -34,7 +39,7 @@ import io.qameta.allure.TmsLink;
 
 //@Listeners({TestngListener.class})
 @Listeners({CustomizedTestListener.class})
-public class SampleTest2 extends WebDriverBaseClass {
+public class IoTPortalTest extends WebDriverBaseClass {
 	
 	public WebDriver driver;
 	
@@ -50,6 +55,29 @@ public class SampleTest2 extends WebDriverBaseClass {
 		Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
 		AllureEnvironmentPropertiesWriter.addEnvironmentItem("Web Browser", caps.getBrowserName() + "driver " + caps.getVersion());
 	}
+	
+	public static void snapshot(TakesScreenshot drivername, String filename)
+	  {
+	      // this method will take screen shot ,require two parameters ,one is driver name, another is file name
+	      
+	    String currentPath = System.getProperty("user.dir"); //get current work folder
+	    System.out.println(currentPath);
+	    File scrFile = drivername.getScreenshotAs(OutputType.FILE);
+	        // Now you can do whatever you need to do with it, for example copy somewhere
+	        try {
+	            System.out.println("save snapshot path is:"+currentPath+"/"+filename);
+	            FileUtils.copyFile(scrFile, new File(currentPath+"\\snapshot\\"+filename));
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            System.out.println("Can't save screenshot");
+	            e.printStackTrace();
+	        } 
+	        finally
+	        {
+	           
+	            System.out.println("screen shot finished");
+	        }
+	  }
 
 	
 	@Test(description="Verify the user login function")
@@ -63,23 +91,38 @@ public class SampleTest2 extends WebDriverBaseClass {
 	@Issue("2209")
 	public void loginTest() throws InterruptedException
 	{
-		
+		Thread.sleep(10000);
+		snapshot((TakesScreenshot)driver,"open_iot_portal.png");
 		int sum = driver.findElements(By.xpath("//span[@class='stat-value']")).size();
 		Assert.assertEquals(sum, 3);
 		String text = driver.findElement(By.xpath("//span[@class='stat-value']")).getText();
 		Assert.assertEquals(text, "6");
-		
 		List<WebElement> seen = driver.findElements(By.xpath("//span[@class='stat-value']"));
-		 
 	    List<String> ids = seen.stream().map(e -> e.getText()).collect(Collectors.toList());
+	    
 		driver.findElement(By.xpath("//span[contains(.,'西门子电气(无锡)有限公司 (3台)')]")).click();
+		Thread.sleep(10000);
+		snapshot((TakesScreenshot)driver,"navigation.png");
+		
 		driver.findElement(By.xpath("//span[contains(.,'陕西杭铣机电制造厂 (2台)')]")).click();
+		Thread.sleep(10000);
+		snapshot((TakesScreenshot)driver,"select_navigation.png");
+		
 		WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("span")));
 		List<WebElement> seen2 = driver.findElements(By.cssSelector("span"));
 		for(WebElement w : seen2) {
 			System.out.println(w.getAttribute("title"));
 		}
+		
+		driver.findElement(By.xpath("//span[contains(.,' 力矩电动机')]")).click();
+		Thread.sleep(10000);
+		snapshot((TakesScreenshot)driver,"click_product.png");
+		
+		driver.findElement(By.xpath("//nz-tree-node[2]/li/span[2]/span")).click();
+		Thread.sleep(10000);
+		snapshot((TakesScreenshot)driver,"check_product_detail.png");
+		
 		
 	}
 	
